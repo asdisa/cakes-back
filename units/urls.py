2e9c2c1b8bfa1280b29a8fc01ab021268c8d9f28
asdisa <asde3211q@gmail.com>
2018-11-25 14:37:27 +0400

    urls reworked

--- units/urls.py
+++ units/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('units/', ListUnitsView.as_view(), name="units"),
+    path('units/', UnitList.as_view(), name="unit-list"),
+    path('units/<int:pk>', UnitDetail.as_view(), name="units-detailed"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- units/views.py
+++ units/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import *
 from .serializers import *
 
 
-class ListUnitsView(generics.ListAPIView):
+class UnitList(generics.ListCreateAPIView):
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class UnitDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Unit.objects.all()
+    serializer_class = UnitSerializer
+ 
+    '''def get_queryset(self):
+        return Unit.objects.all().filter(id=self.request.id)
+'''
