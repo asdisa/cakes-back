c92e953a8f76cb5c83804f0b801be71c7c3ab9dc
asdisa <asde3211q@gmail.com>
2018-12-02 16:01:36 +0400

    yet another attempt

--- units/views.py
+++ units/views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import AllowAny
 from .models import *
 from .serializers import *
+
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    def enforce_csrf(self, request):
+        return None
 
 
 class UnitList(generics.ListCreateAPIView):
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
+    permission_classes = (AllowAny, )
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
 
     def perform_create(self, serializer):
         serializer.save()
@@ -18,4 +28,7 @@ class UnitList(generics.ListCreateAPIView):
 class UnitDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
+    permission_classes = (AllowAny, )
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
 
